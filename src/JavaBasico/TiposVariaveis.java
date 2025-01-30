a6953ef437fc036ce8c3b2a19df93410263880b1
AnaJu <anajuliasilva3304@gmail.com>
2025-01-30 16:24:54 -0300

    docs:tipos de variaveis

---
+++ src/JavaBasico/TiposVariaveis.java
@@ -0,0 +1,15 @@
+package JavaBasico;
+
+public class TiposVariaveis {
+byte b = 100;
+short s = 200;                          //16 bits
+int i = 300;                            //num inteiro
+long l = 14857964758935L;               //num inteiro longo - ex: cep
+float f = 3.14f;                       //valores sem precisão
+double d = 3500.41;                    //valores decimais
+char c = 'A';                          //caracter unico
+boolean bool = true;                  // true or false
+
+final int NUMERO_MAXIMO = 9999;  // valor constante, não pode ser modificado
+// final podem ser usados para definir variaveis constantes
+}
