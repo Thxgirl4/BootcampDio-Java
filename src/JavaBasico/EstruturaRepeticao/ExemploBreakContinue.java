61ccee8c178be2f982d80ba4f606e598afc2f591
AnaJu <anajuliasilva3304@gmail.com>
2025-01-31 11:44:16 -0300

    docs: exemplo de break e continue

--- src/JavaBasico/EstruturaRepeticao/ExemploBreakContinue.java
+++ src/JavaBasico/EstruturaRepeticao/ExemploBreakContinue.java
@@ -1,4 +1,16 @@
 package JavaBasico.EstruturaRepeticao;
 
 public class ExemploBreakContinue {
+    public static void main(String[] args) {
+        for(int num = 1; num <=10; num++) {
+            if(num == 3)
+                break;
+            System.out.println(num);
+        }
+    }
 }
+/*
+O comando break interrompeu o laço,
+já o continue interrompeu somente a iteração atual.
+
+*/
