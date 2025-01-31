2a110f03745374d3376af99a311617341f85dc15
AnaJu <anajuliasilva3304@gmail.com>
2025-01-31 01:05:21 -0300

    docs: exemplo de estrutura condicional ternaria

--- src/JavaBasico/EstruturaCondicionais/ResultadoEscolar.java
+++ src/JavaBasico/EstruturaCondicionais/ResultadoEscolar.java
@@ -1,18 +1,26 @@
 package JavaBasico.EstruturaCondicionais;
 
-//condicional composta
-
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota1 = 5;
 
-        if(nota1 >= 7)
-            System.out.println("Aprovado");
-        else
-            System.out.println("Reprovado");
+        //condicional ternaria
+        int nota = 8;
+
+        String resultado = nota>=7 ? "Aprovado" : nota>=5 && nota<7 ? "Recuperação" : "Reprovado";
+         System.out.println(resultado);
     }
 }
+
+
+//condicional composta
 /* Algumas vezes o nosso programa deverá seguir mais de uma jornada de execução
 condionado a uma regra de negócio,
 este cenário é denominado Estrutura Condicional Composta
+
+        int nota1 = 5;
+
+        if(nota1 >= 7)
+        System.out.println("Aprovado");
+        else
+                System.out.println("Reprovado");
 */
