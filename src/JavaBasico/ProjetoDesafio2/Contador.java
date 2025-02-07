2430288cff09b544436bdb44170e0277b8be4415
AnaJu <anajuliasilva3304@gmail.com>
2025-02-07 20:49:39 -0300

    docs: desafio 2 do modulo Controle de Fluxo

--- src/JavaBasico/ProjetoDesafio2/Contador.java
+++ src/JavaBasico/ProjetoDesafio2/Contador.java
@@ -31,3 +31,9 @@ public class Contador {
         }
     }
 }
+/*
+Crie o projeto DesafioControleFluxo
+Dentro do projeto, crie a classe Contador.java para realizar toda a codificação do nosso programa.
+Dentro do projeto, crie a classe ParametrosInvalidosException que representará a exceção de negócio no sistema.
+
+ */
